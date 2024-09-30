980641cb43490ff722d665e93eaeb723367ce572
Veronika <veronika.pruchova@intelstudios.com>
2024-09-30 19:52:06 +0200

    Pridavam text

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.body.innerHTML = "<h2>Lekce 02 - Příjem divadla</h2>"
+
+document.body.innerHTML += "Jeden lístek do divadla Pěst na oko stojí 12 euro. Spočítejte měsíční příjem divadla ze vstupného, přichází-li průměrně 174 návštěvníků na jedno představení a divadlo hraje 15 představení měsíčně. Uložte výsledek do proměnné prijem. Hodnotu proměnné prijem vypište do stránky. Divadlo se rozhodlo prodávat studentské vstupné ve výši 65 % plného vstupného. Jak se změní měsíční příjem divadla pokud víme, že 40 % návštěvníků jsou studenti? Vypište do stránky i příjem divadla se započítanou slevou pro studenty."
+
+const mena = "euro"
+let listek = 12
+let 
