b2d58cc6ce5a268841328a308d04cd61877c07b2
Veronika <veronika.pruchova@intelstudios.com>
2024-09-30 20:25:10 +0200

    druhy ukol

--- script.js
+++ script.js
@@ -19,4 +19,17 @@ let prijemNestudenti = pocetNavstevniku* (1-procentoStudentu) * listek * pocetPr
 let prijemSeSlevou = prijemStudenti + prijemNestudenti
 document.body.innerHTML += "<p>" + "Měsíční příjem se slevou: " + prijemSeSlevou + " euro" + "</p>"
 
-//ahoj
+
+document.body.innerHTML += "<h2>Lekce 02 - Házení kostkou</h2>"
+
+document.body.innerHTML += "Vymyslete, jak použít funkci Math.random a různé zaokrouhlovací funkce probírané v této lekci k simulování hodu klasickou šestistěnnou kostkou. S použitím vhodných funkcí sestavte výraz, který vygeneruje náhodné celé číslo mezi 1 a 6. Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností. Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže."
+
+let hodKostkou = Math.floor(Math.random() * 6) + 1;
+
+/*	1.	Math.random() generuje náhodné desetinné číslo mezi 0 (včetně) a 1 (mimo).
+	2.	Math.random() * 6 rozšíří rozsah na čísla mezi 0 a 5.9999… .
+	3.	Math.floor() zaokrouhluje výsledek dolů, čímž získáme celé číslo v rozsahu 0 až 5.
+	4.	Přidáním + 1 posuneme rozsah výsledků na 1 až 6, což odpovídá hodu šestihrannou kostkou.
+*/
+
+document.body.innerHTML += "<p>" + "Hod kostkou: " + hodKostkou + "</p>"
